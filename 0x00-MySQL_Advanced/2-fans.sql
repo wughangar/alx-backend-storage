0120293186dc51e8b6dc0ff899942d50bfa83aba
wughangar <wughangar.rose@gmail.com>
2023-12-16 11:16:39 +0300

    fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,7 @@
 -- script that ranks country origins of bands
 -- ordered by nmumber of fans
 
-SELECT origin, SUM(total_fans) as nb_fans
+SELECT origin, SUM(fans) as nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;

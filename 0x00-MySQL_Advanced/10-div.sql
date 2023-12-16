458ec69a783adaaaffcec4d6ce5d4b8f4b867d07
wughangar <wughangar.rose@gmail.com>
2023-12-16 13:21:03 +0300

    safe divide

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,15 @@
+-- script that creates a function SafeDiv that divides
+DELIMITER //
+CRATE FUNCTION SafrDiv(a INT, b INT)
+RETURN INT
+BEGIN
+	DECLARE result INT;
+	IF b <> 0 THEN
+		SET result = a/b;
+	ELSE
+		SET result = 0;
+	END IF;
+	RETURN result;
+END //
+
+DELIMITER ;

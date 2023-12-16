ef12b6cfa06711da985d2749cc7cfee8ef744508
wughangar <wughangar.rose@gmail.com>
2023-12-16 09:46:31 +0300

    best band

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,9 +1,13 @@
 -- script that ranks country origins of bands
 -- ordered by nmumber of fans
 
-SELECT origin, 
-SUM(nb_fans) AS total_fans,
-ROW_NUMBER() OVER (ORDER BY SUM(nb_fans) DESC) AS country_rank
-FROM metal_bands
-GROUP BY origin
-ORDER BY total_fans DESC;
+SELECT
+    origin,
+    SUM(nb_fans) AS total_fans,
+    RANK() OVER (ORDER BY SUM(nb_fans) DESC) AS country_rank
+FROM
+    metal_bands
+GROUP BY
+    origin
+ORDER BY
+    total_fans DESC;

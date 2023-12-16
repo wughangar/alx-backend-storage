8195a821fa0153849cad146b77c47f8fb3a70d7f
wughangar <wughangar.rose@gmail.com>
2023-12-16 13:05:52 +0300

    average score

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,23 @@
+-- script that creates a procedure CompueAverageScoreForUser
+
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser(
+	IN p_user_id INT
+)
+BEGIN
+	DECLARE avg_score DECIMAL(10, 2);
+	SELCT AVG(score) INOT avg_score
+	FROM corrections
+	WHERE user_id = p_user_id;
+
+	IF avg_score IS NOT NULL THEN
+		UPDATE user_scores
+		SET average_score = avg_score
+		WHERE user_id = p_user_id;
+	ELSE
+		INSERT INTO user_scores (user_id, average_score)
+		VALUES (p_user_id, 0);
+	END IF;
+END //
+
+DELIMITER ;

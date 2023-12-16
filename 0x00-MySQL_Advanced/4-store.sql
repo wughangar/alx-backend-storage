3c7e91379528d432ce616db2438d9a23906bff46
wughangar <wughangar.rose@gmail.com>
2023-12-16 12:41:18 +0300

    item name

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -4,4 +4,4 @@
 CREATE TRIGGER updated-quantity
 AFTER INSERT ON orders
 FOR EACH ROW
-	UPDATE items SET quantity = quantity - NEW.number;
+	UPDATE items SET quantity = quantity - NEW.number WHERE NAME = NEW.item_name;

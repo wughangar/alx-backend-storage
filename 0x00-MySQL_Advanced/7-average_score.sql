83051dd54bc0f8c8fd74ba14e241d2a58b2847a3
wughangar <wughangar.rose@gmail.com>
2023-12-16 16:18:00 +0300

    user scores

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -7,16 +7,15 @@ CREATE PROCEDURE ComputeAverageScoreForUser(
 BEGIN
 	DECLARE avg_score DECIMAL(10, 2);
 	SELECT AVG(score) INTO avg_score
-	FROM corrections
+	FROM user_scores
 	WHERE user_id = p_user_id;
 
 	IF avg_score IS NOT NULL THEN
-		UPDATE user_scores
-		SET average_score = avg_score
+		UPDATE user_performance
+		SET average_score_column = avg_score
 		WHERE user_id = p_user_id;
 	ELSE
-		INSERT INTO user_scores (user_id, average_score)
-		VALUES (p_user_id, 0);
+		SELECT 'No scores found for the user' AS Result;
 	END IF;
 END$$
 

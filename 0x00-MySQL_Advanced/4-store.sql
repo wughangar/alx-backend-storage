5d9a2131e3a20cf90d823c9aa2b1587ded6c3cd2
wughangar <wughangar.rose@gmail.com>
2023-12-16 15:55:43 +0300

    delimiter 1663

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,7 +1,12 @@
 -- script that create a trigger that decreases the quantity of an item
 -- after  a new order
-DROP TRIGGER IF EXISTS updated_quantity
+DROP TRIGGER IF EXISTS updated_quantity;
+DELIMITER $$
 CREATE TRIGGER updated_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
-	UPDATE items SET quantity = quantity - NEW.number WHERE NAME = NEW.item_name;
+	UPDATE items
+	SET quantity = quantity - NEW.number
+	WHERE NAME = NEW.item_name;
+$$
+DELIMITER ;

b99a060c813fa1f1a3dcb5ba88b1a5e2b3fcf8e2
wughangar <wughangar.rose@gmail.com>
2023-12-16 16:09:13 +0300

    valid email

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,12 +1,13 @@
 -- script that creates a triggern whne an old email is chnaged and not valid
+DROP TRIGGER IF EXISTS reset_email;
+DELIMITER $$
 
 CREATE TRIGGER reset_email
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
 	IF OLD.email <> NEW.email THEN
-		SET new.VALID_EMAIL = 0;
-	ELSE
-		SET NEW.valid_email = NEW.valid_email;
+		SET NEW.valid_email = 0;
 	END IF;
-END
+END$$
+DELIMITER ;

47711f0c14fa32566a134b04efd9544fa04dfa8f
wughangar <wughangar.rose@gmail.com>
2023-12-16 12:37:53 +0300

    new order

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -4,12 +4,7 @@
 CREATE TRIGGER updated-quantity
 AFTER INSERT ON orders
 FOR EACH ROW
-BEGIN
-	DECLARE item_count INT;
-	SELECT COUNT(*) INTO item_count
-	FROM ordered_items
-	WHERE order_id = NEW.order_id;
 	UPDATE items
-	SET quantity = quantity - item_count
-	WHERE item_id = NEW.item_id;
-END;
+	JOIN orders ON items.NAME = orders.item_name
+	SET items.quantity = items.quantity - orders.number
+	WHERE orders.order_id = NEW.order_id;

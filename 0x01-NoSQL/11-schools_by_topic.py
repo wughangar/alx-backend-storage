963ab5bd8a5af745a3d2d9f2fbfb1699264c52f0
wughangar <wughangar.rose@gmail.com>
2023-12-19 13:12:23 +0300

    changed length

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -12,5 +12,4 @@ def schools_by_topic(mongo_collection, topic):
         mongo_collection - pymongo collection object
         topic - string) will be topic searched
     """
-    schools = mongo_collection.find("topics": topic)
-    return schools
+    return mongo_collection.find("topics": topic)

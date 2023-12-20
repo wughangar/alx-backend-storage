fed028f125f3f9c21b27399c0d4e15999425db95
wughangar <wughangar.rose@gmail.com>
2023-12-20 12:51:48 +0300

    reading from radis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -28,3 +28,27 @@ class Cache:
             data = data.encode('utf-8')
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key: str, fn: Callable = None) -> Union[bytes, str, int]:
+        """
+        takes str and a callable which is used to convert
+        data back to desired format
+        """
+        if not self._redis.exists(key):
+            return None
+        data = self._redis.get(key)
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """
+        method that automatically paremetize cache.get
+        """
+        return self.get(key, fn=lambda x: x.decode('utf-8') if x else None)
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """
+        method that automatically paremetize cache.get
+        """
+        return self.get(key, fn=lambda x: int(x) if x else None)
